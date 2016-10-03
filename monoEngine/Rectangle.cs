ba5984ee159d9ae289a44b59649c12bfbe156587
asojka09 <austinsojka@hotmail.com>
2016-10-03 15:53:05 -0500

    Updated the way tiles work

--- monoEngine/Rectangle.cs
+++ monoEngine/Rectangle.cs
@@ -18,17 +18,18 @@ namespace monogame
 			Texture.SetData (new Color[] { Color.White });
 		}
 
-		public static void Draw(SpriteBatch spriteBatch, Vector2 position, Vector2 scale, Color color, float layerDepth = 0f){
+		public static void Draw(SpriteBatch spriteBatch, Vector2 position, Vector2 scale, Color color, Vector2 origin = default(Vector2), float layerDepth = 0f){
 			if (isValid) {
-				spriteBatch.Draw (Texture, position: position, scale: scale, color: color, layerDepth: layerDepth);
+				spriteBatch.Draw (Texture, position: position, scale: scale, color: color, layerDepth: layerDepth, origin: origin);
 			}
 		}
 
-		public static void Draw(SpriteBatch spriteBatch, float x, float y, float width, float height, Color color, float layerDepth = 0f){
+		public static void Draw(SpriteBatch spriteBatch, float x, float y, float width, float height, Color color, float origin_x = 0f, float origin_y = 0f, float layerDepth = 0f){
 			if (isValid) {
 				Vector2 position = new Vector2 (x, y);
 				Vector2 scale = new Vector2 (width, height);
-				spriteBatch.Draw (Texture, position: position, scale: scale, color: color, layerDepth: layerDepth);
+				Vector2 origin = new Vector2 (origin_x, origin_y);
+				spriteBatch.Draw (Texture, position: position, scale: scale, color: color, layerDepth: layerDepth, origin: origin);
 			}
 		}
 

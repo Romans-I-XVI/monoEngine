4a61616710c6d24d1f05433e6d097e42dd0415d1
asojka09 <austinsojka@hotmail.com>
2016-10-04 17:05:54 -0500

    Switched canvas to be in engine

---
+++ monoEngine/RenderCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+namespace monogame
+{
+	class RenderCanvas : Entity
+	{
+		public RenderTarget2D othersRenderTarget;
+
+		public RenderCanvas(int width, int height) {
+			this.othersRenderTarget = new RenderTarget2D (GameRoot.graphicsDevice, width, height);
+			this.Image = new Image(this.othersRenderTarget);
+		}
+
+	}
+}
+

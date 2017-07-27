11b6f8b582e48e99dcc8dc57e26f850077fe1871
asojka09 <austinsojka@hotmail.com>
2017-07-27 15:34:26 -0500

    Added viewport adapters

--- Engine/ContentHolder.cs
+++ Engine/ContentHolder.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Media;
+
 using MonoGame.Extended.BitmapFonts;
 
 //This should reference what the current project is.

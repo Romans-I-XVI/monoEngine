f58f154b685e5286a7ffa5a70b47ba041aed1515
Austin Sojka <ausitnsojka@hotmail.com>
2018-12-10 12:22:29 -0600

    Added tweener and oscillator objects

--- Engine/Tweens.cs
+++ Engine/Tweens.cs
@@ -2,12 +2,75 @@
 using System.Collections.Generic;
 namespace Engine
 {
-
     public enum Tween
     {
         Linear,
         Sinusoidal
     }
+
+    public class Tweener
+    {
+
+        public Tween Tween;
+        public float Start;
+        public float Dest;
+        public float Current { get { return Tweens.SwitchTween(Tween, Start, Dest, Timer.TotalMilliseconds, Duration); } }
+        public float Duration;
+        public readonly GameTimeSpan Timer = new GameTimeSpan();
+        public bool Done { get { return Timer.TotalMilliseconds >= Duration; } }
+
+        public Tweener(float start, float dest, float duration, Tween tween)
+        {
+            Start = start;
+            Dest = dest;
+            Duration = duration;
+        }
+
+        public void ChangeDest(float dest)
+        {
+            Start = Current;
+            Dest = dest;
+            Timer.Mark();
+        }
+    }
+
+    public class Oscillator
+    {
+        private Tweener _tweener;
+        private bool _forward = true;
+        private float _start;
+        private float _dest;
+
+        public Oscillator(float start, float dest, float duration, Tween tween)
+        {
+            _start = start;
+            _dest = dest;
+            _tweener = new Tweener(start, dest, duration, tween);
+        }
+
+        public float Update()
+        {
+            float current = _tweener.Current;
+
+            if (_tweener.Done)
+            {
+                if (_forward)
+                {
+                    _tweener.ChangeDest(_start);
+                    _forward = false;
+                }
+                else
+                {
+                    _tweener.ChangeDest(_dest);
+                    _forward = true;
+                }
+            }
+
+            return current;
+        }
+
+    }
+
     public static class Tweens
     {
         public static float SwitchTween(Tween tween, float start, float finish, float currentTime, float duration)
